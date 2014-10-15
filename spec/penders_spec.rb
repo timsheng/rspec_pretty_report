8bdaba689dee28fca0f4c5a44aefba5144854918
timsheng <sly19870601@gmail.com>
2014-10-15 13:33:49 +0800

    rspec_pretty_report initialized

---
+++ spec/penders_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+# require_relative '../lib/rspec_pretty_report'
+
+describe 'Pending Examples' do
+
+  it 'should do example stuff' do
+   pending('coming soon')
+  end
+
+end
---
+++ spec/rspec_pretty_report_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+# require_relative '../lib/rspec_pretty_report'
+
+describe 'RspecHtmlFormatter' do
+
+  it 'should do example stuff' do
+    'apple'.should == 'apple'
+  end
+
+end
